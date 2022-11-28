16eef5c753ef57efdbd9d85016be615ba9aaf4d2
Ömer Aykaç <farukomeraykac@gmail.com>
2022-11-28 21:21:36 +0300

    Eureka Configuration

---
+++ MicroService/dicovery-server/src/test/java/com/kodalamaio/dicoveryServer/DicoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodalamaio.dicoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
